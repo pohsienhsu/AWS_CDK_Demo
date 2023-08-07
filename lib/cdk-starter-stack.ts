08e58d0d627d674e72865cf39f3eb0f140c479d6
phsu40 <phsu40@gatech.edu>
2023-08-07 10:36:29 -0700

    Output a L2 bucket name

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-import { Duration } from "aws-cdk-lib";
+import { CfnOutput, Duration } from "aws-cdk-lib";
 import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -34,13 +34,17 @@ export class CdkStarterStack extends cdk.Stack {
       },
     });
 
-    new Bucket(this, "MyL2Bucket", {
+    const MyL2Bucket = new Bucket(this, "MyL2Bucket", {
       lifecycleRules: [
         {
           expiration: Duration.days(2),
         },
       ],
     });
+
+    new CfnOutput(this, 'MyL2BucketName', {
+      value: MyL2Bucket.bucketName
+    })
 
     new L3Bucket(this, "MyL3Bucket", 3);
   }
