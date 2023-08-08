3acc87f3306ac3306bf560c8c91e87ea49d35fc1
phsu40 <phsu40@gatech.edu>
2023-08-08 12:36:07 -0700

    Naming photo bucket name with stack ID suffix

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -1,18 +1,23 @@
 import { Construct } from "constructs";
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
 import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 
 export class PhotosStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
 
-        const myBucket = new Bucket(this, 'PhotosBucket2', {
-            bucketName: 'photosbucket-234kj35'
-        });
+  private stackSuffix: string;
 
-        (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket2234kj34')
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        // create a new resource
-        // delete the old one
-    }
+    this.initalizeSuffix();
+
+    const myBucket = new Bucket(this, "PhotosBucket", {
+      bucketName: `photos-bucket-${this.stackSuffix}`,
+    });
+  }
+
+  private initalizeSuffix() {
+    const shortStackId = cdk.Fn.select(2, cdk.Fn.split("/", this.stackId));
+    this.stackSuffix = cdk.Fn.select(4, cdk.Fn.split("-", shortStackId));
+  }
 }
